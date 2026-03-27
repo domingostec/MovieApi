6d4723549d3bd33c59e88c225145fd3ea779d677
mdpdsantos <matheuspereirasantos10082006@gmail.com>
2026-03-27 10:06:03 -0300

    algumas aalterações no projeto

--- MovieApi/src/main/java/com/domingostec/MovieApi/controller/GlobalExecptionHandler.java
+++ MovieApi/src/main/java/com/domingostec/MovieApi/controller/GlobalExecptionHandler.java
@@ -1,14 +1,19 @@
 package com.domingostec.MovieApi.controller;
 
+import com.domingostec.MovieApi.exceptions.ErrorResponse;
+import com.domingostec.MovieApi.exceptions.MovieExceptions.InvalidTitleExeption;
+import com.domingostec.MovieApi.exceptions.MovieExceptions.MovieNotFoundException;
+import com.domingostec.MovieApi.exceptions.UserExceptions.AccessDeniedException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import com.domingostec.MovieApi.exceptions.MovieExceptions.InvalidGenreException;
 import com.domingostec.MovieApi.exceptions.UserExceptions.InvalidPasswordException;
 import com.domingostec.MovieApi.exceptions.UserExceptions.UserAlreadyExistsExeption;
 import com.domingostec.MovieApi.exceptions.UserExceptions.UserNotFoundException;
+import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
 public class GlobalExecptionHandler {
@@ -29,7 +34,71 @@ public class GlobalExecptionHandler {
     }
 
     @ExceptionHandler(InvalidGenreException.class)
-    public ResponseEntity<String> handleInvalidGenre(InvalidGenreException ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleInvalidGenre(InvalidGenreException ex,
+                                                     HttpServletRequest http){
+
+        ErrorResponse errorResponse = new ErrorResponse(
+
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                http.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidTitleExeption.class)
+    public ResponseEntity<ErrorResponse> handlerInvalidTitle(InvalidTitleExeption ex,
+                                                             HttpServletRequest http){
+        ErrorResponse errorResponse = new ErrorResponse(
+
+        HttpStatus.BAD_REQUEST.value(),
+        HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        ex.getMessage(),
+        http.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handlerAccessDenied(AccessDeniedException ex,
+                                                             HttpServletRequest http){
+        ErrorResponse errorResponse = new ErrorResponse(
+
+        HttpStatus.UNAUTHORIZED.value(),
+        HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+        ex.getMessage(),
+        http.getRequestURI()
+        );
+
+        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlerMovieNotFound(MovieNotFoundException ex,
+                                                             HttpServletRequest http){
+        ErrorResponse errorResponse = new ErrorResponse(
+
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                http.getRequestURI()
+        );
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
+    public ResponseEntity<ErrorResponse> handlerForbiden(HttpClientErrorException.Forbidden ex,
+                                                         HttpServletRequest http){
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                ex.getMessage(),
+                http.getRequestURI()
+        );
+
+        return  ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
     }
 }
